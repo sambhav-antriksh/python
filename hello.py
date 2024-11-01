fb89f420a52439cdca2da5cb8d99101438ec55f0
Sambhav Antriksh <151106109+sambhav-antriksh@users.noreply.github.com>
2024-11-01 04:21:30 +0000

    Commit

--- hello.py
+++ hello.py
@@ -1,3 +1,10 @@
-print("Hello")
-print("This line is added from the replit")
-print("Added a new one for git")
+#Smart way to find is the string a Palindrome
+def is_pal(S):
+  if len(S) == 1:
+    return True
+  if S[0] == S[-1]:
+    return is_pal(S[1 : -1])
+  else:
+    return False
+S='saippuakivikauppias' #a Finnish word meaning a dealer in lye or caustic soda.
+print(is_pal(S))
